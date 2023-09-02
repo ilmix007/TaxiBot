22cd0ddf5b3481add8f0fee760e89f34e2106dec
ilmix007 <imi87@yandex.ru>
2023-09-03 02:00:46 +0500

    added model CustomUser

--- coreapp/models.py
+++ coreapp/models.py
@@ -2,8 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(AbstractUser):
-    pass
+class CustomUser(AbstractUser):
+    tg_id = models.CharField(max_length=63)
 
     def __str__(self):
         return self.username
