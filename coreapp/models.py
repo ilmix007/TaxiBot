688d1c8ca5ba5ed71d05778f0daf6388e83fefcb
ilmix007 <imi87@yandex.ru>
2023-09-03 00:54:29 +0500

    updated model Bot

--- coreapp/models.py
+++ coreapp/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 class Bot(models.Model):
     """Бот"""
-    token = models.CharField(verbose_name='Token', max_length=63)
+    token = models.CharField(verbose_name='Token', max_length=63, unique=True)
     name = models.CharField(verbose_name='Name', max_length=63)
-    about = models.TextField(verbose_name='About', max_length=255)
-    description = models.TextField(verbose_name='Description', max_length=255)
+    about = models.TextField(verbose_name='About', max_length=255,
+                             null=True, blank=True)
+    description = models.TextField(verbose_name='Description', max_length=255,
+                                   null=True, blank=True)
