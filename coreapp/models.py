90398c3431ce5342ab1a3fa092916f14a190c794
ilmix007 <imi87@yandex.ru>
2023-09-03 00:48:58 +0500

    added model Bot

--- coreapp/models.py
+++ coreapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Bot(models.Model):
+    """Бот"""
+    token = models.CharField(verbose_name='Token', max_length=63)
+    name = models.CharField(verbose_name='Name', max_length=63)
+    about = models.TextField(verbose_name='About', max_length=255)
+    description = models.TextField(verbose_name='Description', max_length=255)
