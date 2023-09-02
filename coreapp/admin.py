6effc87390856037b185233e8356d7a587df7625
ilmix007 <imi87@yandex.ru>
2023-09-03 00:55:55 +0500

    added BotAdmin

--- coreapp/admin.py
+++ coreapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from coreapp.models import Bot
+
+
+@admin.register(Bot)
+class BotAdmin(admin.ModelAdmin):
+    list_display = ('name', 'token')
+
