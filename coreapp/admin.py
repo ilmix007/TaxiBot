7d17a630792d8c0720ec77551de90095761ef62d
ilmix007 <imi87@yandex.ru>
2023-09-04 00:28:22 +0500

    updated CustomUserAdmin

--- coreapp/admin.py
+++ coreapp/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from coreapp.models import Bot
+from coreapp.models import Bot, CustomUser
 
 
 @admin.register(Bot)
 class BotAdmin(admin.ModelAdmin):
     list_display = ('name', 'token')
 
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'in_waiting', 'on_the_route',
+                    'last_updated', 'location')
+    list_filter = ('in_waiting', 'on_the_route')
