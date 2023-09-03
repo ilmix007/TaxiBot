5e28a2a529a84e6480cbb795b9ec946681f84c5b
ilmix007 <imi87@yandex.ru>
2023-09-04 00:25:29 +0500

    updated CustomUser

--- coreapp/models.py
+++ coreapp/models.py
@@ -1,9 +1,15 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.gis.geos import Point
 from django.db import models
+from django.contrib.gis.db import models
 
 
 class CustomUser(AbstractUser):
-    tg_id = models.CharField(max_length=63)
+    tg_id = models.CharField(max_length=63, blank=True, null=True)
+    location = models.PointField(blank=True, null=True)
+    in_waiting = models.BooleanField(default=False)
+    on_the_route = models.BooleanField(default=False)
+    last_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.username
